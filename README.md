7a064f12a53715711457cac9348514944dfe622a
RamyaBalakumaar <ramyabr0106@gmail.com>
2023-08-13 17:08:45 +0530

    allure report changes completed

--- README.md
+++ README.md
@@ -40,6 +40,15 @@ URL - https://restful-booker.herokuapp.com/apidoc/index.html
 #### If module error, use below cmd to execute the script
 `python -m pytest -n auto tests/integration_test/ -s -v --html=report.html`
 
+### How to generate allure report?
+- Step1: Install node.js in the system - this will install npm (node package manager)
+- Step2: `open cmd -> check node --version`, if available Install allure-commandline which is a package manager by using npm
+`npm i -g allure-commendline` which helps to generate a report.
+- Step3: pycharm teminal - `pip install allure-pytest`
+- Step4: Mark the test cases with annotations like 
+  @allure.feature()
+  @allure.story()
+
 
 ### How to Run via Jenkins?
 
--- tests/integration_test/integration_test/test_integration_tc1.py
+++ tests/integration_test/integration_test/test_integration_tc1.py
@@ -1,4 +1,5 @@
 import pytest
+import allure
 
 from src.constants.apiconstants import url_create_token, url_create_booking, url_delete_booking, url_update_booking
 from src.helpers.api_wrapper import post_request, put_request, delete_request
@@ -9,6 +10,8 @@ from src.helpers.utils import common_headers, headers_withToken, common_auth
 
 class TestIntegration(object):
 
+    @pytest.mark.integration
+    @allure.feature("#TC1 creating a token")
     def test_create_token(self):
         global token
         response = post_request(url_create_token(), headers=common_headers(), auth=None, payload=common_auth(),
@@ -18,6 +21,8 @@ class TestIntegration(object):
         verify_token(token)
         return token
 
+    @pytest.mark.integration
+    @allure.feature("#TC2 creating a bookingID")
     def test_create_booking(self):
         global booking_id
         # call the functions to make request
@@ -30,6 +35,8 @@ class TestIntegration(object):
         verify_key(booking_id)
         return booking_id
 
+    @pytest.mark.integration
+    @allure.feature("#TC3 updating full data in created bookingID")
     def test_update_booking(self):
         response = put_request(url_update_booking(booking_id),
                                headers=headers_withToken(token), auth=None,
@@ -37,6 +44,8 @@ class TestIntegration(object):
         verify_http_status_code(response, 200)
         print(response.text)
 
+    @pytest.mark.integration
+    @allure.feature("#TC4 deleting the created bookingID")
     def test_delete_booking(self):
         response = delete_request(url_delete_booking(booking_id),
                                   headers=headers_withToken(token), auth=None, in_json=False)
